472a0bcc3119994ac9db22a0012e44aef6de85d8
Canis Artorus <acjames148@gmail.com>
2021-09-21 10:35:59 -0600

    Version 10 label.

--- src/main/java/com/github/canisartorus/prospectorjournal/ProspectorJournal.java
+++ src/main/java/com/github/canisartorus/prospectorjournal/ProspectorJournal.java
@@ -4,13 +4,13 @@ package com.github.canisartorus.prospectorjournal;
  * @author Alexander James
  * @author Gregorious Techneticies
  * @author Dyonovan
- * 
- * 
+ *
+ *
  * Core file for the Prospector's Journal mod.
  * This keeps track of ore bearing rocks, and indicator flowers,
- * in order to make waypointing everywhere unnecessary 
+ * in order to make waypointing everywhere unnecessary
  * to find the large ore Veins as are present in GT6.
- * 
+ *
  *  Built primarily off of TCNodeTracker at https://github.com/Dyonovan/TCNodeTracker
  *  and thus remains under Creative Commons CC-BY-NC-SA4.0 (attribution, non-commercial, share-alike)
  */
@@ -27,26 +27,26 @@ import com.github.canisartorus.prospectorjournal.lib.*;
 import com.github.canisartorus.prospectorjournal.network.*;
 import java.util.ArrayList;
 import java.util.List;
-  
+
 @cpw.mods.fml.common.Mod(modid=ProspectorJournal.MOD_ID, name=ProspectorJournal.MOD_NAME, version=ProspectorJournal.VERSION, dependencies="required-after:gregapi_post; required-after:gregtech; after:ImmersiveEngineering")
 public final class ProspectorJournal extends gregapi.api.Abstract_Mod {
 	/** Your Mod-ID has to be LOWERCASE and without Spaces. Uppercase Chars and Spaces can create problems with Resource Packs. This is a vanilla forge "Issue". */
-	public static final String MOD_ID = "prospectorjournal"; 
+	public static final String MOD_ID = "prospectorjournal";
 	/** This is your Mods Name */
-	public static final String MOD_NAME = "ProspectorJournal"; 
+	public static final String MOD_NAME = "ProspectorJournal";
 	/** This is your Mods Version */
-	public static final String VERSION = "ProspectorJournal-MC1710-0.9.2"; 
+	public static final String VERSION = "ProspectorJournal-MC1710-0.10.0"; 
 	/** Contains a ModData Object for ID and Name. Doesn't have to be changed. */
 	public static gregapi.code.ModData MOD_DATA = new gregapi.code.ModData(MOD_ID, MOD_NAME);
 
 	@cpw.mods.fml.common.SidedProxy(modId = MOD_ID, clientSide = "com.github.canisartorus.prospectorjournal.ProxyClient", serverSide = "com.github.canisartorus.prospectorjournal.ProxyServer")
     public static ProxyServer PROXY;
-	
+
 	/*
 	@cpw.mods.fml.common.Mod.Instance(MOD_ID)
 	public static ProspectorJournal instance;
 	*/
-	
+
 	public static String hostName = "ProspectorJournal";
 	public static boolean doGui = false;
 	public static int xMarker, yMarker, zMarker;
@@ -74,20 +74,20 @@ public final class ProspectorJournal extends gregapi.api.Abstract_Mod {
 	public void onModPreInit2(FMLPreInitializationEvent aEvent) {
 		// Make new items, add them to OreDicts, and do recipes using only internal items.
 		ConfigHandler.init(aEvent.getSuggestedConfigurationFile());
-		
+
 //		new gregapi.block.multitileentity.MultiTileEntityRegistry("ca.pj.multitileentity");
 //		if(ConfigHandler.makeBook)
 			Items.RegisterItems();
-		
-		Utils.NW_PJ = new NetworkHandler(MOD_ID, "CAPJ", 
+
+		Utils.NW_PJ = new NetworkHandler(MOD_ID, "CAPJ",
 				new ChatPacket(0), new ChatPacket(1), new ChatPacket(2),
-				new PacketOreSurvey(0), new PacketOreSurvey(1), new PacketOreSurvey(2), new PacketOreSurvey(3), new PacketOreSurvey(4), new PacketOreSurvey(5), new PacketOreSurvey(6), new PacketOreSurvey(7), 
+				new PacketOreSurvey(0), new PacketOreSurvey(1), new PacketOreSurvey(2), new PacketOreSurvey(3), new PacketOreSurvey(4), new PacketOreSurvey(5), new PacketOreSurvey(6), new PacketOreSurvey(7),
 				new PacketVoidVein(0), new PacketVoidVein(1), new PacketVoidVein(2), new PacketVoidVein(3));
-				
+
 		net.minecraftforge.common.MinecraftForge.EVENT_BUS.register(new RightClickEvent());
         cpw.mods.fml.common.FMLCommonHandler.instance().bus().register(new ClientConnectionEvent());
         getProxy().registerKeybindings();
-        
+
         AVAILABLE_TRACKERS.add(new OreMenuData());
         AVAILABLE_TRACKERS.add(new BedrockMenuData());
         if (gregapi.data.MD.IE.mLoaded)
@@ -98,35 +98,35 @@ public final class ProspectorJournal extends gregapi.api.Abstract_Mod {
 	public void onModInit2(FMLInitializationEvent aEvent) {
 		// Init gets the recipes that took oredict entries, or otherwise things from other mods to build.
 		new Dwarf().run();
-		
+
 //		if(ConfigHandler.makeBook)
 			Items.RegisterRecipes();
 		if(ConfigHandler.applyPatches)
 			com.github.canisartorus.prospectorjournal.compat.GtPatches.onInit();
 		getProxy().initKeybinds();
 	}
-	
+
 	@Override
 	public void onModPostInit2(FMLPostInitializationEvent aEvent) {
 		// Insert your PostInit Code here and not above
 		getProxy().registerPointer();
 	}
-	
+
 	@Override
 	public void onModServerStarting2(FMLServerStartingEvent aEvent) {
 		// Insert your ServerStarting Code here and not above
 	}
-	
+
 	@Override
 	public void onModServerStarted2(FMLServerStartedEvent aEvent) {
 		// Insert your ServerStarted Code here and not above
 	}
-	
+
 	@Override
 	public void onModServerStopping2(FMLServerStoppingEvent aEvent) {
 		// Insert your ServerStopping Code here and not above
 	}
-	
+
 	@Override
 	public void onModServerStopped2(FMLServerStoppedEvent aEvent) {
 		// Insert your ServerStopped Code here and not above
